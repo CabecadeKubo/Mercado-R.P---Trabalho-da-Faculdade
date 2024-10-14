f07c54d4ad4622d9d357e04d4e58ae6065dffb6c
CabecadeKubo <kaynansouza06@gmail.com>
2024-10-14 12:35:37 -0300

    first commit

---
+++ simuladordecompra.js
@@ -0,0 +1,15 @@
+/*function alterarQuantidade(valor) {
+    var inputQuantidade = document.getElementById('quantidade');
+    var quantidade = parseInt(inputQuantidade.value);
+    quantidade += valor;
+    if (quantidade < 1) quantidade = 1;
+    inputQuantidade.value = quantidade;
+}*/
+
+function compra (valor){
+
+var quantidade = document.getElementById('contador');
+var preço = document.getElementById('preco')
+
+
+}
